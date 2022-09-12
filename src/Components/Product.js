d5ccffb619547e644797fe39aed7455dd74b3557
Mauro <maurodirosa19@gmail.com>
2022-09-13 01:39:02 +0200

    fetch

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 export default function Product(props) {
-  const { title, price, description } = props;
+  const { name, price, description } = props;
   return (
     <div>
-      <h4>{title}</h4>
-      <p>price:{price}</p>
+      <h4>{name || ""}</h4>
+      <p>price:{price || ""}</p>
       <p>description:{description || ""} </p>
       <button>Order</button>
     </div>
--- src/Components/Products.js
+++ src/Components/Products.js
@@ -6,27 +6,26 @@ export default function Products() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch("https://api.mercadolibre.com/sites/MLA/search?q=ipod#options")
+    fetch("https://api-nodeservice.herokuapp.com/products")
       .then((res) => res.json())
       .then((data) => {
-        setProductsList(data.results);
+        setProductsList(data);
         setLoading(false);
-        console.log("data", data);
+        console.log("data", data.results);
       })
       .catch((e) => {
         console.log(e);
       });
   }, []);
-
   if (loading) {
-    return <div>Loading...</div>;
+    return "loading...";
   } else {
     return (
       <div>
         <h1>Products</h1>
         {productsList.map((productOfList) => (
           <Product
-            title={productOfList.title}
+            name={productOfList.name}
             price={productOfList.price}
             description={productOfList.description}
           />
